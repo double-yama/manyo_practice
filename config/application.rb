21444503ab5aa3c05a13b9fd9637f249d1dd95e9
Shunsuke Yamaguchi <s.yamaguchi@ashita-team.com>
2018-04-20 11:46:26 +0900

    sakimura

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Manyo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    # config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    config.autoload_paths += %W(#{config.root}/app/uploaders)
   end
 end
