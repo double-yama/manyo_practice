13ac5333d5ec62784c31055268f6315c6968a383
Shunsuke Yamaguchi <s.yamaguchi@ashita-team.com>
2018-06-05 14:51:40 +0900

    migration file delete

--- db/migrate/20180312012507_add_column_tasks.rb
+++
@@ -1,4 +0,0 @@
-class AddColumnTasks < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20180601002130_create_task_groups.rb
+++ db/migrate/20180601002130_create_task_groups.rb
@@ -1,8 +1,8 @@
 class CreateTaskGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :task_groups do |t|
-      t.references :task, foreign_key: true
-      t.references :group, foreign_key: true
+      t.integer :task_id, foreign_key: true
+      t.integer :group_id, foreign_key: true
 
       t.timestamps
     end
